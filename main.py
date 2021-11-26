e43a339bad35ade53b1e5854625bc1fe1d4389f5
pablito <paz300014@gmail.com>
2021-11-26 18:41:32 -0300

    algo mas

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ print ("buenas tardes")
 
 print ("iniciando programa")
 
-<<<<<<< HEAD
 print ("===!!!!")
-=======
-print("===========")
 
->>>>>>> 0a7ddf27f74812f4d01a0dba58469a7bf46a1af5
+print("0. salir")
+print("1 ver cantidad de cafe")
