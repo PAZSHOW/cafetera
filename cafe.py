7bb5155a1a80980bcaa71da82343a330685b8fe0
pablito <paz300014@gmail.com>
2021-11-26 18:55:29 -0300

    metodo llenar

--- cafe.py
+++ cafe.py
@@ -1,10 +1,9 @@
 Class Cafetera:
     def __init__(self):
         self._capacidad_maxima=1000
-<<<<<<< HEAD
-        self._cant_actual = 0
-=======
         self._cant_actual = 0
 
+    def llamar cafetera(self):
+        sel.cant_actual = self._cantidad_maxima
+
         
->>>>>>> main
